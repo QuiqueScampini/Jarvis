752fc8608b2efb468739b8970d0410df4b742150
QuiqueScampini <enriquescampini@gmail.com>
2017-11-16 21:24:19 -0300

    IDK #yourMom

--- server.sh
+++ server.sh
@@ -4,34 +4,34 @@ workdir=`pwd`/app
  
 start() {
     mv Jarvis.log Jarvis.old.log
-	echo "Starting mjpg_streamer - `date '+%d/%m/%Y %H:%M:%S'`"
-	/usr/local/bin/mjpg_streamer -i "/usr/local/lib/input_uvc.so" -o "/usr/local/lib/output_http.so -w /usr/local/www -p 8090" &
-	echo "mjpg_streamer Started - `date '+%d/%m/%Y %H:%M:%S'`"
-	echo "Starting pigpiod - `date '+%d/%m/%Y %H:%M:%S'`"
-	sudo pigpiod
-	echo "pigpiod Started - `date '+%d/%m/%Y %H:%M:%S'`"
-	echo "Starting Server - `date '+%d/%m/%Y %H:%M:%S'`"
+    echo "Starting mjpg_streamer - `date '+%d/%m/%Y %H:%M:%S'`"
+    /usr/local/bin/mjpg_streamer -i "/usr/local/lib/input_uvc.so" -o "/usr/local/lib/output_http.so -w /usr/local/www -p 8090" &
+    echo "mjpg_streamer Started - `date '+%d/%m/%Y %H:%M:%S'`"
+    echo "Starting pigpiod - `date '+%d/%m/%Y %H:%M:%S'`"
+    sudo pigpiod
+    echo "pigpiod Started - `date '+%d/%m/%Y %H:%M:%S'`"
+    echo "Starting Server - `date '+%d/%m/%Y %H:%M:%S'`"
     python3 -u Jarvis.py &
     echo "Server started."
 }
 
 stop() {
     pid=`ps -ef | grep 'Jarvis.py' | grep -v grep | awk '{ print $2 }'`
-    echo $pid
-    kill $pid
+    echo ${pid}
+    kill ${pid}
     sleep 2
-	echo "Stopping Server - `date '+%d/%m/%Y %H:%M:%S'`"
+    echo "Stopping Server - `date '+%d/%m/%Y %H:%M:%S'`"
     echo "Server killed."
 
-	pid=`ps -ef | grep 'mjpg_streamer' | grep -v grep | awk '{ print $2 }'`
-	echo $pid
-    kill $pid
+    pid=`ps -ef | grep 'mjpg_streamer' | grep -v grep | awk '{ print $2 }'`
+    echo ${pid}
+    kill ${pid}
 	echo "Stopping mjpg_streamer - `date '+%d/%m/%Y %H:%M:%S'`"
     echo "mjpg_streamer killed."
 
-	pid=`ps -ef | grep 'pigpiod' | grep -v grep | awk '{ print $2 }'`
-	echo $pid
-    sudo kill $pid
+    pid=`ps -ef | grep 'pigpiod' | grep -v grep | awk '{ print $2 }'`
+    echo ${pid}
+    sudo kill ${pid}
 	echo "Stopping pigpiod - `date '+%d/%m/%Y %H:%M:%S'`"
     echo "pigpiod killed."
 }
