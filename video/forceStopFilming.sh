c5560aea99c4d9add4f1f3356920b9886402bd5c
QuiqueScampini <enriquescampini@gmail.com>
2017-11-19 01:04:14 -0300

    Film Framework #yourMom

--- video/forceStopFilming.sh
+++ video/forceStopFilming.sh
@@ -9,19 +9,28 @@ kill -9 ${actualProcessPid}
 if [ -f FILM_PID ]; then
     pid=`cat FILM_PID`
     kill ${pid}
-    wait ${pid}
+    ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
+	while [ $? -eq 0 ];do
+		ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
+	done
 fi
 
 if [ -f STITCH_PID ]; then
     pid=`cat STITCH_PID`
     kill ${pid}
-    wait ${pid}
+    ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
+	while [ $? -eq 0 ];do
+		ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
+	done
 fi
 
 if [ -f CONVERT_PID ]; then
     pid=`cat CONVERT_PID`
     kill ${pid}
-    wait ${pid}
+    ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
+	while [ $? -eq 0 ];do
+		ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
+	done
 fi
 
 rm -f FILM_PID STITCH_PID CONVERT_PID
