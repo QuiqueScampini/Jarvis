aae9dc2a2328b4a188cce9fd2e772fadab14789a
QuiqueScampini <enriquescampini@gmail.com>
2017-11-19 01:11:16 -0300

    Film Framework #yourMom

--- video/forceStopFilming.sh
+++ video/forceStopFilming.sh
@@ -9,30 +9,37 @@ kill -9 ${actualProcessPid}
 if [ -f FILM_PID ]; then
     pid=`cat FILM_PID`
     kill ${pid}
-    ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
+    ps -ef | grep ${pid} | grep ffmpeg | grep -v grep > /dev/null
 	while [ $? -eq 0 ];do
 		ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
 	done
+
+    rm -f FILM_PID
 fi
+
 
 if [ -f STITCH_PID ]; then
     pid=`cat STITCH_PID`
     kill ${pid}
-    ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
+    ps -ef | grep ${pid} | grep ffmpeg | grep -v grep > /dev/null
 	while [ $? -eq 0 ];do
 		ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
 	done
+
+	rm -f STITCH_PID
 fi
+
+
 
 if [ -f CONVERT_PID ]; then
     pid=`cat CONVERT_PID`
     kill ${pid}
-    ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
+    ps -ef | grep ${pid} | grep ffmpeg | grep -v grep > /dev/null
 	while [ $? -eq 0 ];do
 		ps -ef | grep ${pid} | grep ffmpeg | grep -v grep
 	done
-fi
 
-rm -f FILM_PID STITCH_PID CONVERT_PID
+	rm -f CONVERT_PID
+fi
 
 exit 0
